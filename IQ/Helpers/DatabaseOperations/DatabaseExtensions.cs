944f71f36762b0774b889f80870e4f80a714ced1
cfdaniel <cfdaniel@skiff.com>
2023-09-22 02:55:12 +0100

    Added Automated Table Creation in Db

--- IQ/Helpers/DatabaseOperations/DatabaseExtensions.cs
+++ IQ/Helpers/DatabaseOperations/DatabaseExtensions.cs
@@ -65,5 +65,37 @@ namespace IQ.Helpers.DatabaseOperations
             bool isSuperUser = (bool)queryUserStatusCommand.ExecuteScalar();
             return isSuperUser;
         }
+
+        public static bool TriggerDbMassAction_Branch()
+        {
+            bool isCompleted;
+            try
+            {
+                string createSalesTable = "CREATE TABLE IF NOT EXISTS Sales (\r\n    InvoiceID INT UNIQUE NOT NULL,\r\n    ModelID VARCHAR(255) NOT NULL,\r\n    BrandID VARCHAR(255) NOT NULL,\r\n    QuantitySold INT NOT NULL,\r\n    SellingPrice FLOAT NOT NULL,\r\n    SoldTo VARCHAR(255) NOT NULL,\r\n    CustomerContactInfo VARCHAR(255) NOT NULL,\r\n    SoldOn TIMESTAMP NOT NULL\r\n);";
+                using var createSalesTableCommand = new NpgsqlCommand(createSalesTable, con);
+                string createPurchasesTable = "CREATE TABLE IF NOT EXISTS Purchases (\r\n    InvoiceID INT UNIQUE NOT NULL,\r\n    ModelID VARCHAR(255) NOT NULL,\r\n    BrandID VARCHAR(255) NOT NULL,\r\n    QuantityBought INT NOT NULL,\r\n    BuyingPrice FLOAT NOT NULL,\r\n    PurchasedFrom VARCHAR(255) NOT NULL,\r\n    SupplierContactInfo VARCHAR(255) NOT NULL,\r\n    PurchasedOn TIMESTAMP NOT NULL\r\n);";
+                using var createPurchasesTableCommand = new NpgsqlCommand(createPurchasesTable, con);
+                string createTransferInwardsTable = "CREATE TABLE IF NOT EXISTS TransferInwards (\r\n    TransferID INT UNIQUE NOT NULL,\r\n    ModelID VARCHAR(255) NOT NULL,\r\n    BrandID VARCHAR(255) NOT NULL,\r\n    QuantityTransferred INT NOT NULL,\r\n    TransferredFrom VARCHAR(255) NOT NULL,\r\n    SignedBy VARCHAR(255) NOT NULL,\r\n    TransferredOn TIMESTAMP NOT NULL\r\n);";
+                using var createTransferInwardsTableCommand = new NpgsqlCommand(createTransferInwardsTable, con);
+                string createTransferOutwardsTable = "CREATE TABLE IF NOT EXISTS TransferOutwards (\r\n    TransferID INT UNIQUE NOT NULL,\r\n    ModelID VARCHAR(255) NOT NULL,\r\n    BrandID VARCHAR(255) NOT NULL,\r\n    QuantityTransferred INT NOT NULL,\r\n    TransferredTo VARCHAR(255) NOT NULL,\r\n    SignedBy VARCHAR(255) NOT NULL,\r\n    TransferredOn TIMESTAMP NOT NULL\r\n);";
+                using var createTransferOutwardsTableCommand = new NpgsqlCommand(createTransferOutwardsTable, con);
+                string createReturnInwardsTable = "CREATE TABLE IF NOT EXISTS ReturnInwards (\r\n    ReturnID INT UNIQUE NOT NULL,\r\n    ModelID VARCHAR(255) NOT NULL,\r\n    BrandID VARCHAR(255) NOT NULL,\r\n    QuantityReturned INT NOT NULL,\r\n    ReturnedFrom VARCHAR(255) NOT NULL,\r\n    SignedBy VARCHAR(255) NOT NULL,\r\n    ReturnedOn TIMESTAMP NOT NULL\r\n);";
+                using var createReturnInwardsTableCommand = new NpgsqlCommand(createReturnInwardsTable, con);
+                string createReturnOutwardsTable = "CREATE TABLE IF NOT EXISTS ReturnOutwards (\r\n    ReturnID INT UNIQUE NOT NULL,\r\n    ModelID VARCHAR(255) FORIEGN KEY NOT NULL,\r\n    BrandID VARCHAR(255) NOT NULL,\r\n    QuantityReturned INT NOT NULL,\r\n    ReturnedTo VARCHAR(255) NOT NULL,\r\n    SignedBy VARCHAR(255) NOT NULL,\r\n    ReturnedOn TIMESTAMP NOT NULL\r\n);";
+                using var createReturnOutwardsTableCommand = new NpgsqlCommand(createReturnOutwardsTable, con);
+                string createBranchInventoryTable = "CREATE TABLE IF NOT EXISTS BranchInventory (\r\n    ModelID VARCHAR(255) UNIQUE NOT NULL,\r\n    BrandID VARCHAR(255) NOT NULL,\r\n    Description VARCHAR(255) NOT NULL,\r\n    QuantityInStock INT NOT NULL,\r\n    UnitPrice FLOAT NOT NULL,\r\n    TotalWorth FLOAT NOT NULL,\r\n);";
+                using var createBranchInventoryTableCommand = new NpgsqlCommand(createBranchInventoryTable, con);
+                string createCommitHistoryTable = "CREATE TABLE IF NOT EXISTS CommitHistory (\r\n    CommitID INT UNIQUE NOT NULL,\r\n    CommitDescription VARCHAR(255) NOT NULL,\r\n    CommitDate TIMESTAMP NOT NULL,\r\n    ApprovalStatus VARCHAR(255) NOT NULL,\r\n    ApprovalDate TIMESTAMP \r\n);";
+                using var createCommitHistoryTableCommand = new NpgsqlCommand(createCommitHistoryTable, con);
+
+                isCompleted = true;
+            }
+            catch
+            {
+                isCompleted = false;
+            }
+
+            return isCompleted;
+        }
     }
 }
---
+++ IQ/Views/WarehouseViews/WarehouseWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using IQ.Views.WarehouseViews.Pages.Inventory;
+using IQ.Views.WarehouseViews.Pages.ReturnOutwards;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.ViewManagement;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace IQ.Views.WarehouseViews
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class WarehouseWindow : Window
+    {
+        public WarehouseWindow()
+        {
+            this.InitializeComponent();
+            ExtendsContentIntoTitleBar = true;
+            SetTitleBar(WarehouseTitleBar);
+            ApplicationView.PreferredLaunchViewSize = new Size(1200, 840);
+            ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
+        }
+
+        private void WarehouseViewItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+        {
+            if (args.IsSettingsInvoked)
+            {
+                //contentFrame.Navigate(typeof(SettingsPage));
+            }
+            else
+            {
+                // Handle regular menu item invocation
+                var itemContent = args.InvokedItemContainer;
+                if (itemContent != null)
+                {
+                    switch (itemContent.Name)
+                    {
+                        case "ReturnOutwardsPage":
+                            contentFrame.Navigate(typeof(ReturnOutwardsPage));
+                            break;
+
+                        case "WarehouseInventoryPage":
+                            contentFrame.Navigate(typeof(WarehouseInventoryPage));
+                            break;
+                    }
+                }
+            }
+
+        }
+        private void WarehouseNavLoaded(object sender, RoutedEventArgs e)
+        {
+            // set the initial SelectedItem
+            foreach (NavigationViewItemBase item in WarehouseNavigator.MenuItems)
+            {
+                if (item is NavigationViewItem && item.Tag.ToString() == "InventoryPage")
+                {
+                    WarehouseNavigator.SelectedItem = item;
+                    break;
+                }
+            }
+            contentFrame.Navigate(typeof(WarehouseInventoryPage));
+        }
+        private void ContentFrame_NavigationFailed(Object sender, NavigationFailedEventArgs args)
+        {
+
+        }
+    }
+}
