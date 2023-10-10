36153083cc1b25f50831693a3ae201594b40bf22
cfdaniel <cfdaniel@skiff.com>
2023-10-10 15:34:47 +0100

    Safety Commit

--- IQ/Views/WarehouseViews/WarehouseWindow.xaml.cs
+++ IQ/Views/WarehouseViews/WarehouseWindow.xaml.cs
@@ -7,7 +7,14 @@ using IQ.Views.WarehouseViews.Pages.ReturnOutwards;
 using IQ.Views.WarehouseViews.Pages.TransferInwards;
 using IQ.Views.WarehouseViews.Pages.TransferOutwards;
 using Microsoft.UI.Xaml;
+using Octokit;
 using Microsoft.UI.Xaml.Controls;
+using System.Reflection;
+using System;
+using System.Threading.Tasks;
+using Microsoft.UI.Xaml.Media;
+using Windows.UI;
+using Aspose.Pdf.Operators;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -95,8 +102,56 @@ namespace IQ.Views.WarehouseViews
             WindowExtensions.Logout(this);
         }
 
-        private void CheckUpdates_Click(object sender, RoutedEventArgs e)
+        private async Task<bool> CheckUpdates_Click(object sender, RoutedEventArgs e)
         {
+            try
+            {
+                var github = new GitHubClient(new ProductHeaderValue("IQ"));
+                var releases = await github.Repository.Release.GetAll("DotSynth", "IQ");
+
+                if (releases.Count > 0)
+                {
+                    var latestRelease = releases[0]; // Get the latest release
+                    var latestVersion = new Version(latestRelease.TagName);
+
+                    // Compare the latest version with the current app version
+                    var currentVersion = Assembly.GetExecutingAssembly().GetName().Version;
+                    if (latestVersion > currentVersion)
+                    {
+                        // A new version is available
+                        return true;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle exceptions (e.g., network error)
+                ShowCompletionAlertDialogAsync($"Error checking for updates: {ex.Message}", this);
+            }
+
+            return false;
+        }
+
+        public static async void ShowCompletionAlertDialogAsync(string alert, Window m)
+        {
+            // Create a ContentDialog
+            ContentDialog alertDialog = new ContentDialog
+            {
+                // Set the title, content, and close button text
+                Title = "Alert",
+                Content = alert,
+                CloseButtonText = "OK"
+            };
+
+            // Set the foreground to hex color #020066
+            alertDialog.Foreground = new SolidColorBrush(Color.FromArgb(255, 2, 0, 102));
+
+            // Set the XamlRoot property to the same as an element in the app window
+            // For example, if you have a StackPanel named MyPanel in your XAML
+            alertDialog.XamlRoot = m.Content.XamlRoot;
+
+            // Show the ContentDialog and get the result
+            ContentDialogResult result = await alertDialog.ShowAsync();
 
         }
 
@@ -107,31 +162,38 @@ namespace IQ.Views.WarehouseViews
             await PDFOperations.CreateWarehousePurchasesPdfForMonth(this);
             await PDFOperations.CreateWarehouseTInsPdfForMonth(this);
             await PDFOperations.CreateWarehouseRInsPdfForMonth(this);
+
+            ShowCompletionAlertDialogAsync("Exports Complete", this);
         }
 
         private async void ExportTOutsButton_Click(object sender, RoutedEventArgs e)
         {
             await PDFOperations.CreateWarehouseTOutsPdfForMonth(this);
+            ShowCompletionAlertDialogAsync("Export Complete", this);
         }
 
         private async void ExportROutsButton_Click(object sender, RoutedEventArgs e)
         {
             await PDFOperations.CreateWarehouseROutsPdfForMonth(this);
+            ShowCompletionAlertDialogAsync("Export Complete", this);
         }
 
         private async void ExportPurchasesButton_Click(object sender, RoutedEventArgs e)
         {
             await PDFOperations.CreateWarehousePurchasesPdfForMonth(this);
+            ShowCompletionAlertDialogAsync("Export Complete", this);
         }
 
         private async void ExportTInsButton_Click(object sender, RoutedEventArgs e)
         {
             await PDFOperations.CreateWarehouseTInsPdfForMonth(this);
+            ShowCompletionAlertDialogAsync("Export Complete", this);
         }
 
         private async void ExportRInsButton_Click(object sender, RoutedEventArgs e)
         {
             await PDFOperations.CreateWarehouseRInsPdfForMonth(this);
+            ShowCompletionAlertDialogAsync("Export Complete", this);
         }
     }
 }
